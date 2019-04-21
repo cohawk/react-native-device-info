5b1114722e8e6edcfb6026f76583b5a9d0399eb6
cohawk <bpcohee@gmail.com>
2019-04-21 13:44:11 -0400

    Added web deviceid placeholder

--- web/index.js
+++ web/index.js
@@ -5,4 +5,8 @@ var RNDeviceInfo = require('../default');
 
 RNDeviceInfo.userAgent = window.navigator.userAgent;
 
+RNDeviceInfo.deviceId = 'react-native-web-device-uuid-placeholder';
+
+// http://localhost:4001/api/authnz/sessions/new/2d56ea33-8a0e-482a-92f2-9bd1df6bb963
+
 module.exports = RNDeviceInfo;
